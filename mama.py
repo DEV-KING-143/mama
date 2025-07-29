44c045690c7f2f93da6b3c052dfeb1ad173f0bc2
DEV KUSHWAHA <144660077+DEV-KING-143@users.noreply.github.com>
2025-07-29 19:27:05 +0530

    Create mama.py

---
+++ mama.py
@@ -0,0 +1,17 @@
+import os, sys, platform
+
+
+bit = platform.architecture()[0]
+if "32bit" in bit:
+    os.system("clear")
+    print(" your device is 32bit ")
+    print(" tool not supported ")
+elif "64bit" in bit:
+    os.system("clear")
+    print(" Welcome mama Tool .... ")
+    import mama64
+else:
+    os.system("clear")
+    print(" unknown arch ")
+    sys.exit()
+  
